4fb400beef9d6ac4b04a505220cc4b20a5a38a41
Jarrett Horton <jghorton14@gmail.com>
2020-07-04 07:46:14 -0400

    messsing with styling

--- src/features/intro/Intro.jsx
+++ src/features/intro/Intro.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Intro = () => {
   return (
     <Container style={{ marginTop: "3%" }}>
-      <h2>Welcome to Complain Tracker.</h2>
+      <h2>Welcome to Complain Tracker</h2>
       <p>
         Do you someone who complains a lot and gets on your ever last nerve? If
         so, feel free to use this responsive webapp to track how often people
         complain.
       </p>
-      {/* TODO add login button */}
+      <p>
+        Click here to <Link to="/login">Login</Link> and start tracking your
+        complaints!
+      </p>
     </Container>
   );
 };
