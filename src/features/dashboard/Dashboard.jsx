1c4985510ab13434c9a0cd79ec611de258eba7bd
Jarrett Horton <jghorton14@gmail.com>
2020-07-11 08:47:04 -0400

    Improving Dashboard

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,27 +1,32 @@
 import React, { useState, useReducer } from "react";
 import { connect } from "react-redux";
+import { Card, CardBody, CardTitle, CardText, Container } from "reactstrap"
+import { Link } from "react-router-dom";
 
 import NavigationBar from "../navbar/Navbar";
 import { getLocalStorageObject } from "../../utils/localstorage";
 import { useEffect } from "react";
 
-/**
- * We have two states....
- *
- * 1) unAuthorized..
- * 2) aUTHORIZED...
- */
 const Dashboard = ({ login }) => {
-  console.log("Dashboard login", login);
 
   useEffect(() => {
+    // Fix this...
     console.log(getLocalStorageObject());
   }, []);
 
   return (
-    <>
-      <p>Dashboard</p>
-    </>
+    <Container style={{ marginTop: '2%' }}>
+      {/* Charts and graphs */}
+      <Link to="/add-compliment" style={{textDecoration: 'none', color: 'black', }}>
+        <Card style={{ cursor: 'pointer', borderRadius: '.5rem' }} className="box-shadow">
+          <CardBody>
+            <CardTitle style={{fontSize:"1.5rem"}}>Add a Complaint</CardTitle>
+            <CardText>Someone urking you or really getting on your nerves? Indulge by tracking how often they piss you off. </CardText>
+          </CardBody>
+        </Card>
+      </Link>
+
+    </Container>
   );
 };
 
